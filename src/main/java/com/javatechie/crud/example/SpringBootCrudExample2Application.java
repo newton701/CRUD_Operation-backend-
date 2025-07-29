5f3a1f2e3d55c10dcffd98e0a214df692933f9a6
newton701 <newtondakua701@gmail.com>
2025-07-30 00:04:48 +0530

    added the logger and swagger implementation

--- src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -1,9 +1,12 @@
 package com.javatechie.crud.example;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title = "Product API",version = "2.0",description = "product microservice"))
 public class SpringBootCrudExample2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.javatechie.crud.example.controller;
 
 import com.javatechie.crud.example.entity.Product;
 import com.javatechie.crud.example.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ProductController {
 
@@ -14,37 +16,56 @@ public class ProductController {
     private ProductService service;
 
     @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product) {
+    public Product addProduct(@RequestBody Product product)
+    {
         return service.saveProduct(product);
+
     }
+
 
     @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products) {
+    public List<Product> addProducts(@RequestBody List<Product> products)
+    {
+        log.info("entering to addproducts with values :{} ",products);
         return service.saveProducts(products);
+
     }
+
 
     @GetMapping("/products")
-    public List<Product> findAllProducts() {
+    public List<Product> findAllProduct()
+    {
+        log.info("entering into findAllProduct method in the controller");
+
         return service.getProducts();
+
     }
 
+
     @GetMapping("/productById/{id}")
-    public Product findProductById(@PathVariable int id) {
+    public Product findProductById(@PathVariable int id)
+    {
         return service.getProductById(id);
     }
 
-    @GetMapping("/product/{name}")
-    public Product findProductByName(@PathVariable String name) {
+
+    @GetMapping("/productByName/{name}")
+    public Product findProductByName(@PathVariable String name)
+    {
         return service.getProductByName(name);
     }
 
+
     @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product) {
+    public Product updateProduct(@RequestBody Product product)
+    {
         return service.updateProduct(product);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable int id) {
+
+    @DeleteMapping ("/delete/{id}")
+    public String deleteProduct(@PathVariable int id)
+    {
         return service.deleteProduct(id);
     }
 }
